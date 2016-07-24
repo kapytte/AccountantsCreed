e51b086e572a0542c8e2ea3875d86ec35947e3cc
kapytte <1002418@student.sae.edu.au>
2016-07-24 17:52:27 +0800

    doc upload 2

---
+++ GDS230-Documents/Scripts/MultipleChoice.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Text;
+using System.IO;
+
+
+
+public class MultipleChoice : MonoBehaviour 
+{
+	public Text Question; 
+ 	public Button a, b, c, d;
+
+	public string textName; 
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		StreamReader txtReader = StreamReader (Application.persistentDataPath + "/" + textName);
+		availableQuestions = TextReader.
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void ReadText()
+	{
+
+
+	}
+
+	void OptionA()
+	{
+		
+	}
+
+	void OptionB()
+	{
+
+	}
+
+	void OptionC()
+	{
+
+	}
+
+	void OptionD()
+	{
+
+	}
+
+
+}
